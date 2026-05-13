b8bc655caad2ad07f010b4df372c7518bc0d0cab
Ravalika <ravalika09@gmail.com>
2026-05-13 06:04:12 +0000

    sub/multi-Ravalika

--- myapp.java
+++ myapp.java
@@ -2,5 +2,7 @@ public class Msg {
 	public static void main(String[] args){
 		int a=10, int b=20;
 		System.out.println("Sum is:",+(a+b));
+		System.out.println("Sub is:",+(a-b));
+		System.out.println("Multi is:",+(a*b));
 	}
 }
